8cc9b670c0ee3209630e895a07ab31a81648a959
IgorSilvaZZ <igor06silva12@gmail.com>
2022-04-18 15:25:37 -0300

    Add vue cli

---
+++ componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="contador">
+    <span>{{ contador }}</span>
+    <button @click="adicionar">+</button>
+    <button @click="subtrair">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContadorComponent",
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    adicionar() {
+      this.contador++;
+    },
+    subtrair() {
+      this.contador--;
+    },
+  },
+};
+</script>
+
+<style>
+.contador span {
+  height: 30px;
+
+  border-bottom: 1px solid #ccc;
+
+  padding: 5px 30px;
+}
+
+.contador button {
+  height: 30px;
+  width: 30px;
+
+  margin-left: 10px;
+
+  border-radius: 10px;
+
+  background: coral;
+
+  outline: none;
+}
+</style>
